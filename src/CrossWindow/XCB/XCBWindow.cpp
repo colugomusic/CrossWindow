d531050a8a093261f9bbddbe1b0be27b7d10d75f
Colugo <colugomusic@gmail.com>
2024-07-28 17:44:39 +0100

    xcb impl

--- src/CrossWindow/XCB/XCBWindow.cpp
+++ src/CrossWindow/XCB/XCBWindow.cpp
@@ -1,42 +1,70 @@
 #include "XCBWindow.h"
 
-namespace xwin
-{
-Window::Window() {}
+namespace xwin {
 
-bool Window::create(const WindowDesc& desc, EventQueue& eventQueue)
-{
-    const XWinState& xwinState = getXWinState();
-    mConnection = xwinState.connection;
-    mScreen = xwinState.screen;
-
-    mXcbWindowId = xcb_generate_id(mConnection);
-
-    uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-    uint32_t value_list[2] = {
-        mScreen->black_pixel,
-        XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS |
-            XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION |
-            XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW |
-            XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE};
-
-    xcb_create_window(mConnection, XCB_COPY_FROM_PARENT, mXcbWindowId,
-                      mScreen->root, desc.x, desc.y, desc.width, desc.height, 0,
-                      XCB_WINDOW_CLASS_INPUT_OUTPUT, mScreen->root_visual, mask,
-                      value_list);
-
-    xcb_map_window(mConnection, mXcbWindowId);
-
-    const unsigned coords[] = {static_cast<unsigned>(desc.x),
-                               static_cast<unsigned>(desc.y)};
-    xcb_configure_window(mConnection, mXcbWindowId,
-                         XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, coords);
-
-    xcb_flush(mConnection);
-
-    return true;
+Window::~Window() {
+	if (mConnection != nullptr) {
+		destroy();
+	}
 }
 
-void Window::close() { xcb_destroy_window(mConnection, mXcbWindowId); }
+auto Window::create(const WindowDesc& desc, EventQueue& eventQueue, void* parentWindow) -> bool {
+	const XWinState& xwinState = getXWinState();
+	mConnection = xwinState.connection;
+	mScreen = xwinState.screen;
 
+	mXcbWindowId = xcb_generate_id(mConnection);
+	const auto parent_window_id = reinterpret_cast<xcb_window_t>(parentWindow);
+
+	uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+	uint32_t value_list[2] = {
+		mScreen->black_pixel,
+		XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS |
+			XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION |
+			XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW |
+			XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE};
+
+	xcb_create_window(mConnection, XCB_COPY_FROM_PARENT, mXcbWindowId, parent_window_id,
+					  desc.x, desc.y, desc.width, desc.height, 0,
+					  XCB_WINDOW_CLASS_INPUT_OUTPUT, mScreen->root_visual, mask,
+					  value_list);
+
+	xcb_map_window(mConnection, mXcbWindowId);
+
+	const unsigned coords[] = {static_cast<unsigned>(desc.x), static_cast<unsigned>(desc.y)};
+	xcb_configure_window(mConnection, mXcbWindowId, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, coords);
+
+	// Set the child window to always be on top of the parent window
+	xcb_configure_window(mConnection, mXcbWindowId, XCB_CONFIG_WINDOW_STACK_MODE, (uint32_t[]){XCB_STACK_MODE_ABOVE});
+
+	xcb_flush(mConnection);
+
+	return true;
 }
+
+void Window::destroy() {
+	xcb_destroy_window(mConnection, mXcbWindowId);
+}
+
+auto Window::get_size(unsigned* width, unsigned* height) -> void {
+	// Get the window size
+	xcb_get_geometry_cookie_t geometry_cookie = xcb_get_geometry(mConnection, mXcbWindowId);
+	xcb_get_geometry_reply_t* geometry_reply = xcb_get_geometry_reply(mConnection, geometry_cookie, nullptr);
+	if (geometry_reply) {
+		*width = geometry_reply->width;
+		*height = geometry_reply->height;
+		free(geometry_reply);
+	}
+}
+
+auto Window::set_position(unsigned x, unsigned y) -> void {
+	// Set the window position
+	xcb_configure_window(mConnection, mXcbWindowId, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, (uint32_t[]){x, y});
+}
+
+auto Window::set_size(unsigned width, unsigned height) -> void {
+	// Set the window size
+	xcb_configure_window(mConnection, mXcbWindowId, XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT, (uint32_t[]){width, height});
+}
+
+} // namespace xwin
--- src/CrossWindow/XCB/XCBWindow.h
+++ src/CrossWindow/XCB/XCBWindow.h
@@ -4,30 +4,29 @@
 #include "../Common/Init.h"
 #include "../Common/WindowDesc.h"
 
+#include <any>
 #include <xcb/xcb.h>
 
-namespace xwin
-{
-/**
- * XCB windows are pretty straight forward and easy, simple virtual function
- * calls:
- *
- * https://xcb.freedesktop.org/manual/group__XCB__Core__API.html
- */
-struct Window
-{
-public:
-    Window();
+namespace xwin {
 
-    // Initialize this window with the XCB API.
-    bool create(const WindowDesc& desc, EventQueue& eventQueue);
-
-    void close();
-
-  protected:
-    xcb_connection_t* mConnection = nullptr;
-    xcb_screen_t* mScreen = nullptr;
-    unsigned mXcbWindowId = 0;
-    unsigned mDisplay = 0;
+struct Window {
+	Window() = default;
+	~Window();
+	[[nodiscard]] auto get_client_data() -> std::any { return client_data; }
+	[[nodiscard]] auto get_native_handle() -> void* { return (void*)(mXcbWindowId); }
+	[[nodiscard]] auto create(const WindowDesc& desc, EventQueue& eventQueue, void* parentWindow) -> bool;
+	[[nodiscard]] auto is_valid() const -> bool { return bool(mXcbWindowId); }
+	auto destroy() -> void;
+	auto get_size(unsigned* width, unsigned* height) -> void;
+	auto set_client_data(std::any data) -> void { client_data = data; }
+	auto set_position(unsigned x, unsigned y) -> void;
+	auto set_size(unsigned width, unsigned height) -> void;
+protected:
+	xcb_connection_t* mConnection = nullptr;
+	xcb_screen_t* mScreen = nullptr;
+	unsigned mXcbWindowId = 0;
+	unsigned mDisplay = 0;
+	std::any client_data;
 };
-}
+
+} // namespace xwin
